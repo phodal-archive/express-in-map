ba9bb0b0e134af112c57deaa763b1655b19099c6
Fengda HUANG <h@phodal.com>
2016-08-18 07:53:29 +0800

    add basic parse data function

---
+++ demo-parse.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var file = __dirname + '/demo-parse-data.json';
+var _ = require('lodash');
+
+fs.readFile(file, 'utf8', function (err, expressInfos) {
+    if (err) {
+        console.log('Error: ' + err);
+        return;
+    }
+
+    expressInfos = JSON.parse(expressInfos);
+
+    var data = {
+        get: {},
+        transport: [],
+        end: {}
+    };
+
+    _.each(expressInfos, function (express) {
+        var statusDescription = express.StatusDescription;
+
+        if (statusDescription.indexOf('已揽收') !== -1) {
+            data.get = express;
+        }
+        if (statusDescription.indexOf('已发出,下一站') !== -1) {
+            data.transport.push(express);
+        }
+        if (statusDescription.indexOf('已签收') !== -1) {
+            data.end = express;
+        }
+    });
+
+    console.dir(data);
+});
